af18948563865ad68c1cbf5f1e45c828914e558b
Xuemei Luo <xluo@ml6349-XLUO.local>
2022-07-27 17:31:29 -0400

    fix parameter and update calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 cd $1
 
+find . -regex '.*\.bam$' -exec samtools flagstat {} \;
 find . -regex '.*\.fusions.tsv$' -exec md5sum {} \;
 find . -regex '.*\.fusions.discarded.tsv$' -exec md5sum {} \;
 find . -regex '.*\.fusions.pdf$' -exec sh -c "cat {} | grep -av Date | md5sum" \;
