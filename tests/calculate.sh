ed8b6b2c8e8ac132e92bbee2aa252942a398ef57
Xuemei Luo <xluo@ml6349-XLUO.local>
2022-07-28 09:11:43 -0400

    add module

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 cd $1
 
+module load samtools/1.9 2>/dev/null
 find . -regex '.*\.bam$' -exec samtools flagstat {} \;
 find . -regex '.*\.fusions.tsv$' -exec md5sum {} \;
 find . -regex '.*\.fusions.discarded.tsv$' -exec md5sum {} \;
